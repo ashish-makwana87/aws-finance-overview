74cfb1f71526a8154010568cf2438152f959b431
ashish-makwana87 <ashishn.makwana@gmail.com>
2026-01-01 19:17:05 +0530

    feat: Sharp lamba layer for image resizing

--- src/functions/imageResizeHandler.js
+++ src/functions/imageResizeHandler.js
@@ -8,37 +8,67 @@ import sharp from "sharp";
 const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 export const handler = async (event) => {
+  console.log("Lambda triggered");
+  console.log("Received records:", event.Records?.length);
+
   for (const record of event.Records) {
     const bucket = record.s3.bucket.name;
     const key = decodeURIComponent(record.s3.object.key);
 
-    if (!key.startsWith("avatars/original/")) continue;
+    console.log("Processing object");
+    console.log("Bucket:", bucket);
+    console.log("Key:", key);
 
-    const image = await s3.send(
-      new GetObjectCommand({ Bucket: bucket, Key: key })
-    );
+    if (!key.startsWith("avatars/original/")) {
+      console.log("Skipping non-avatar object");
+      continue;
+    }
 
-    const buffer = Buffer.from(await image.Body.transformToByteArray());
+    try {
+      console.log("Downloading image from S3");
 
-    const optimized = await sharp(buffer)
-      .resize(256, 256, {
-        fit: "cover",
-        position: "center",
-      })
-      .webp({ quality: 80 })
-      .toBuffer();
+      const image = await s3.send(
+        new GetObjectCommand({ Bucket: bucket, Key: key })
+      );
 
-    const optimizedKey = key
-      .replace("avatars/original/", "avatars/optimized/")
-      .concat(".webp");
+      const buffer = Buffer.from(await image.Body.transformToByteArray());
 
-    await s3.send(
-      new PutObjectCommand({
-        Bucket: bucket,
-        Key: optimizedKey,
-        Body: optimized,
-        ContentType: "image/webp",
-      })
-    );
+      console.log("Image downloaded. Size (bytes):", buffer.length);
+      console.log("Starting image resize with Sharp");
+
+      // resizing and compression
+      const optimized = await sharp(buffer)
+        .resize(256, 256, {
+          fit: "cover",
+          position: "center",
+        })
+        .webp({ quality: 80 })
+        .toBuffer();
+
+      console.log(
+        "Image resized successfully. Optimized size (bytes):",
+        optimized.length
+      );
+
+      const optimizedKey = key
+        .replace("avatars/original/", "avatars/optimized/")
+        .concat(".webp");
+
+      console.log("Uploading optimized image to:", optimizedKey);
+
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: bucket,
+          Key: optimizedKey,
+          Body: optimized,
+          ContentType: "image/webp",
+        })
+      );
+
+      console.log("Optimized image upload completed");
+    } catch (error) {
+      console.error("Error processing image:", error);
+      throw error;
+    }
   }
 };
